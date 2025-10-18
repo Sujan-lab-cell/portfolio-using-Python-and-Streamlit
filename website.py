0871869686652d79d7dbf9c40034c0e1d31e938c
Your Name <your_github_email@example.com>
2025-10-19 00:48:56 +0530

    your message

--- website.py
+++ website.py
@@ -1,3 +1,7 @@
-print("hello world")
-print("sujan")
-print("this is sujan's portfolio website")
+import streamlit as st
+
+st.set_page_config(page_title="My Portfolio",page_icon=":tada:",layout="wide")
+
+st.subheader("Hi, I am Sujan K S :wave:")
+st.title("A Machine Learning Enthusiast from India :india:")
+st.write("I am passionate about leveraging machine learning to solve real-world problems and create impactful solutions. With a strong foundation in data analysis, model development, and deployment, I enjoy exploring new technologies and methodologies in the field of artificial intelligence,im also Intrested in Natural Language Processing and Deep Learning,IoT etc.")
