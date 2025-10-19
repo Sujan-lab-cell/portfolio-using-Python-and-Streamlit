8f9117aee2d5f5d62c6ed1ddb738e746f998acd0
Your Name <your_github_email@example.com>
2025-10-19 16:17:05 +0530

    your message

--- website.py
+++ website.py
@@ -1,5 +1,6 @@
 import streamlit as st
-import requests 
+import requests as r
+from PIL import Image
 from streamlit_lottie import st_lottie
 st.set_page_config(page_title="My Portfolio",page_icon=":tada:",layout="wide")
 import json
@@ -10,9 +11,18 @@ from streamlit_lottie import st_lottie
 def load_lottiefile(filepath: str):
     with open(filepath, "r") as f:
         return json.load(f)
+
+#use local css
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>",unsafe_allow_html=True)
+local_css("style/style.css")
 lootile_codeing=load_lottiefile("Robot Futuristic Ai animated.json")
 cam=load_lottiefile("Headphone with blueberry cartoon.json")
 st_lottie(cam,height=200,width=300,key="codings")
+# Load images
+img_contact_from2=Image.open("bank.jpg")
+img_contact_from=Image.open("travel.jpg")
 st.header("Welcome to My Portfolio :tada:")
 with st.container():
     st.divider()
@@ -26,7 +36,6 @@ with st.container():
         left_column,right_column=st.columns(2)
         with left_column:
             st.header("About Me")
-            st.write("##")
             st.write(
                 """
                 - I am Sujan K S, a dedicated machine learning enthusiast from India.Driven by the power of  data to drive innovation and solve problems.
@@ -42,3 +51,43 @@ with st.container():
             
     with right_column:
             st_lottie(lootile_codeing,height=400,width=700,key="coding")
+            
+with st.container():
+    st.write("---")
+    st.header("MY Projects")
+    st.write("##")
+    image_column,text_column=st.columns((1,2))
+    with image_column:
+        st.image(img_contact_from)
+    with text_column:
+        st.subheader("Travel Mate AI")
+        st.write(
+            """Developed an AI-powered travel assistant that recommends destinations, plans itineraries, and provides real-time travel insights using user preferences and location data. Integrated NLP for conversational interaction and machine learning for personalized suggestions."""
+        )
+    st.write("##")
+    image_column,text_column=st.columns((1,2))
+    with image_column:
+        st.image(img_contact_from2)
+    with text_column:
+        st.subheader("Banking Management System")
+        st.write(
+            """Banking Management System is a Java-based DBMS project using Swing and AWT for UI, and MySQL for the database. It supports login, account creation with personal details, and basic banking operations like deposit, withdrawal, PIN change, and viewing transaction history."""
+        )  
+   
+   
+with st.container():
+    st.write("---")
+    st.header("Contact Me")
+    st.write("##")
+    contact_form="""<form action="https://formsubmit.co/gowdasujan396@email.com" method="POST">
+     <input type="hidden" name="_captcha" value="false">
+     <input type="text" name="name" placeholder="Your Name" required>
+     <input type="email" name="email" placeholder="Your Email" required>
+     <textarea name="message" placeholder="Your Message Here" required></textarea>
+     <button type="submit">Send</button>
+</form>"""
+left_column,right_column=st.columns(2)
+with left_column:
+    st.markdown(contact_form,unsafe_allow_html=True)
+with right_column:
+    st.empty()
